38bea16204a764d14e82b1ca5db654ec9c832b18
ykl1 <lyoungkwang.yl@gmail.com>
2024-04-07 01:11:26 -0400

    Base Server files, initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import { YoutubeTranscript } from "./index.js";
+import cors from 'cors'
+
+const app = express();
+const port = 3000;
+
+app.use(cors('chrome-extension://akfpaknmiiacecdbdfophehkkibnckce'));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my server!');
+});
+
+app.get('/captions/:videoId', (req, res) => {
+  console.log(req.params.videoId)
+  let captions = YoutubeTranscript.fetchTranscript(req.params.videoId).then(console.log);
+
+  res.json(captions);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
